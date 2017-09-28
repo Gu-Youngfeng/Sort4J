d795f8da912a3e34857ac71abf52f9c786b2fc1a
Gu-yongfeng <yongfeng_gu@163.com>
2017-09-28 14:00:09 +0800

    File Addition
    Add the MainEntry.java as the entry of the project.

---
+++ src/sklse/yongfeng/sort/MainEntry.java
@@ -0,0 +1,17 @@
+package sklse.yongfeng.sort;
+
+/***
+ * <p>This class <b>MainEntry</b> provide the entry of the project.</p>
+ * @author yongfeng
+ * @date Sep.28th, 2017
+ *
+ */
+public class MainEntry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, Java!");
+	}
+
+}
